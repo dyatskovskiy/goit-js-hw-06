a3f87d934039f5ce307d24ad9598e7ea8930c9e2
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-08-17 20:27:31 +0300

    finish 1st hw

---
+++ js/task-01.js
@@ -0,0 +1,12 @@
+const categories = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categories.length}`);
+
+//=======================================
+
+categories.forEach(category => {
+    const categoryName = category.firstElementChild;
+    console.log(`Category: ${categoryName.textContent}`);
+
+    const categoryListOfItems = category.lastElementChild.children;
+    console.log(`Elements: ${categoryListOfItems.length}`)
+    })
