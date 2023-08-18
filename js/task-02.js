634a78647a359679b8a0822e0ffcc989aee2701b
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-08-18 12:19:06 +0300

    finished 2nd hw

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const ingredientItems = ingredients.map(ingredient => {
+  const ingredientEl = document.createElement('li');
+  ingredientEl.classList.add('item');
+  ingredientEl.textContent = ingredient;
+  return ingredientEl;
+})
+
+ingredientsList.append(...ingredientItems);
